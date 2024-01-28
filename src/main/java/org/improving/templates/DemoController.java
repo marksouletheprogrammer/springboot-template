9923f7313eb0d7909a1993fcff193c9897a3fd78
Mark Soule <marksouletheprogrammer@gmail.com>
2024-01-27 18:38:33 -0600

    Very basic project setup

--- src/main/java/org/improving/templates/DemoController.java
+++ src/main/java/org/improving/templates/DemoController.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/customers")
+@RequestMapping(value = "/demo")
 public class DemoController {
 
     @GetMapping
